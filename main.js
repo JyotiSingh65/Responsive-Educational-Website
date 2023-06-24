d42986ea459216c34c7b92f2a08c628fb2c91a78
Jyoti <136021694+JyotiSingh65@users.noreply.github.com>
2023-06-24 15:06:12 +0530

    uploading the files

---
+++ main.js
@@ -0,0 +1,54 @@
+// change navbar style on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0)
+})
+
+
+// show and hide the faq answers
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+        //change icon
+
+        const icon=faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus"
+        }
+        else{
+            icon.className = "uil uil-plus"
+        }
+    })
+})
+
+
+// show /hide nav menu 
+
+const menu =document.querySelector(".nav__menu");
+const menuBtn =document.querySelector("#open-menu-btn");
+const CloseBtn =document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click', ()=> {
+    menu.style.display= "flex";
+    CloseBtn.style.display="inline-block";
+    menuBtn.style.display="none";
+
+})
+
+//close nav menu
+
+const closeNav = () => {
+    menu.style.display = "none";
+    CloseBtn.style.display ="none";
+    menuBtn.style.display = "inline-block";
+}
+
+CloseBtn.addEventListener('click',closeNav);
+
+
+
